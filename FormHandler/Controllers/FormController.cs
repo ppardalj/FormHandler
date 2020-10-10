77a302f9c423dd1575593ff2d67965745e250482
Pedro Pardal <ppardalj@gmail.com>
2020-10-10 10:52:02 +0200

    Form handler controller

---
+++ FormHandler/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FormHandler.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FormController : ControllerBase
+    {
+        private readonly ILogger<FormController> _logger;
+
+        public FormController(ILogger<FormController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("hello")]
+        public string Hello()
+        {
+            return "Hello";
+        }
+
+        [HttpPost("{formId}/submit")]
+        public IActionResult Submit(string formId)
+        {
+            _logger.LogInformation($"Submitted form with id {formId}");
+
+            if (formId != "06332e35-9e64-4bbc-82b6-2a7f94be6b06")
+            {
+                return NotFound();
+            }
+
+            return RedirectPermanent("http://localhost:1313/gracias");
+        }
+    }
+}
